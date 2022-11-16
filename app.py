3ee24def5a13ecf36322fce30f2e9af6264b0f85
Daniel97er <108468246+Daniel97er@users.noreply.github.com>
2022-11-16 15:27:09 +0000

    IDE Start up

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, flash, session
+from flask_session import Session
+from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+
+# Flask instance configuration
+app = Flask(__name__)
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.secret_key = "super secret key"
+
+# Database started
+#db = SQL("sqlite:///database.db")
+
+
+# Homepage / Purchase documentation
+@app.route("/")
+def index():
+
+    # Get global data from database
+    return render_template("index.html")
