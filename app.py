ed0e595b4c3ca4b56356ce7eefdef3c6941cffe8
Daniel97er <108468246+Daniel97er@users.noreply.github.com>
2022-11-17 16:14:28 +0000

    Start with miller-rabin-test function

--- app.py
+++ app.py
@@ -19,5 +19,26 @@ def index():
     return render_template("index.html")
 
 # Miller-Rabin-Test 
-# @app.route("/miller_rabin_test") 
-def miller_rabin_test():
+@app.route("/miller_rabin_test") 
+def miller_rabin_test(): 
+
+  # Miller-Rabin primality test (probabilistic primality test)
+  # Based on the principle of prime modulo and that X*X=1 has maximum only
+  # two solutions one and minus one 
+
+  def miller_rabin_tester(number):
+    # Miller-Rabin-Test 
+    # If the result is negative then it is definitely not a prime number. 
+    # If the result is positive then possibly a false positive result 
+
+    prime_help = number - 1
+    counter = 0
+
+    # Divide prime_help by two, while it is even
+    # and increase the counter each time
+    while prime_help % 2 == 0:
+      prime_help //= 2
+      counter += 1
+
+    # Get random number between 2 and number 
+    random_number = randrange(2, number)
