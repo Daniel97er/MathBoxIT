2e9ae4c2763831200651fc4050a818017efff7f9
Daniel97er <108468246+Daniel97er@users.noreply.github.com>
2022-11-20 12:25:36 +0000

    prime_factors function

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math
 from random import randrange
 from flask import Flask, render_template, request, redirect, flash
 
@@ -96,4 +97,75 @@ def miller_rabin_test():
   else:
     # Render page without result
     return render_template("miller_rabin_test.html")
+
+# Prime factors
+@app.route("/prime_factors", methods=["GET", "POST"])
+def prime_factors():
+
+    def prime_factorization(number):
+        # Return three lists with prime factorials, exponents and one index list for jinja 
+  
+        # Check zero and one by default
+        if number == 0:
+           return [0], [1]
+
+        if number == 1:
+  	       return [1], [1]
+
+        # Create two lists for prime factors and exponents
+        factor_list = []
+        exponent_list = []
+
+        # Get prime numbers up to number 
+        prime_list = prime_test(number)
+  
+        # Go through primes and check if it is a prime factor and possibly add to prime factors list
+        for i in prime_list:
+          if number % i == 0:
+            factor_list.append(i)
+
+        # Calculate the number of exponent for the respective prime factor
+        for i in factor_list:
+          counter = 0
+          temp_number = number 
+          # Keep dividing the number until it no longer fits in
+          while temp_number % i == 0:
+            temp_number //= i
+            counter += 1
+          exponent_list.append(counter) 
+
+        # Create index list for jinja template for loop
+        index_list = [i for i in range(len(factor_list))]
+  
+        return factor_list,exponent_list, index_list
+
+
+    def prime_test(number):
+        # Return list of primes up to number (Sieve of Eratosthenes)
+
+        prime_test_list = []
+
+        # Go through all numbers 
+        for i in range(2, number+1):
+
+         # Set tester to false to check if current i is prime
+         tester = False 
+    
+         # Go up to square root because of efficiency 
+         for j in range(2,int(math.sqrt(i))+1):
+             # If there is a dividend so i is not prime 
+             if i % j == 0:
+                 tester = True 
+                 # Stop loop if i is not prime
+                 break
+    
+         # If tester in the end of the loop is false so i is a prime
+         if tester == False:
+             prime_test_list.append(i)
+
+  
+        return prime_test_list
+    
+    p_factors, p_exponents, index_list = prime_factorization(2)
+    return 
 
