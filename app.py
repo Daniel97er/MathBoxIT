8f20e2960012b2d8f307ce9f69f723ae4ae0884a
Daniel97er <108468246+Daniel97er@users.noreply.github.com>
2022-11-23 19:10:42 +0000

    app.py update

--- app.py
+++ app.py
@@ -19,84 +19,84 @@ def index():
 @app.route("/miller_rabin_test", methods=["GET", "POST"])
 def miller_rabin_test():
 
-  if request.method == "POST":
+    if request.method == "POST":
 
-    # Get data from registration.html
-    number = int(request.form.get("prime"))
-    
-    # Miller-Rabin primality test (probabilistic primality test)
-    # Based on the principle of prime modulo and that X*X=1 has maximum only
-    # two solutions one and minus one
+      # Get data from registration.html
+      number = int(request.form.get("prime"))
+      
+      # Miller-Rabin primality test (probabilistic primality test)
+      # Based on the principle of prime modulo and that X*X=1 has maximum only
+      # two solutions one and minus one
 
-    def miller_rabin_tester(number):
-        # Miller-Rabin-Test
-        # If the result is negative then it is definitely not a prime number. 
-        # If the result is positive then possibly a false positive result 
+      def miller_rabin_tester(number):
+          # Miller-Rabin-Test
+          # If the result is negative then it is definitely not a prime number. 
+          # If the result is positive then possibly a false positive result 
 
-        prime_help = number - 1
-        counter = 0
+          prime_help = number - 1
+          counter = 0
 
-        # Divide prime_help by two, while it is even
-        # and increase the counter each time
-        while prime_help % 2 == 0:
-          prime_help //= 2
-          counter += 1
+          # Divide prime_help by two, while it is even
+          # and increase the counter each time
+          while prime_help % 2 == 0:
+            prime_help //= 2
+            counter += 1
 
-        # Get random number between 2 and number
-        random_number = randrange(2, number)
+          # Get random number between 2 and number
+          random_number = randrange(2, number)
 
-        # When random_number power of prime_help  modulo number comes out equal to one, 
-        # in the first try, it is most likely a prime number
-        if (random_number ** prime_help) % number == 1:
-          return True
-
-        # While counter greater zero test if result is -1 or 1 and decide then
-        while counter > 0:
-          if (random_number ** prime_help) % number == number -1:
+          # When random_number power of prime_help  modulo number comes out equal to one, 
+          # in the first try, it is most likely a prime number
+          if (random_number ** prime_help) % number == 1:
             return True
 
-          if (random_number ** prime_help) % number == 1:
-            return False
-    
-          random_number **= 2
-          counter -= 1
+          # While counter greater zero test if result is -1 or 1 and decide then
+          while counter > 0:
+            if (random_number ** prime_help) % number == number -1:
+              return True
 
-        # Return false if no 1 or -1 appears
-        return False
+            if (random_number ** prime_help) % number == 1:
+              return False
+      
+            random_number **= 2
+            counter -= 1
 
-
-    def miller_rabin_tester_test(number):
-        # Runs the miller-rabin-test 15 times to minimize
-        # the chance of false positive results
-
-        # Return false if number is apparently not prime
-        if number < 2 or number % 2 == 0:
+          # Return false if no 1 or -1 appears
           return False
 
-       # Run Miller-Rabin-test 15 times to test
-        for i in range(15):
-          if not miller_rabin_tester(number):
-            # Return false if number not prime
+
+      def miller_rabin_tester_test(number):
+          # Runs the miller-rabin-test 15 times to minimize
+          # the chance of false positive results
+
+          # Return false if number is apparently not prime
+          if number < 2 or number % 2 == 0:
             return False
 
-       # Return true when most likely prime
-        return True
+        # Run Miller-Rabin-test 15 times to test
+          for i in range(15):
+            if not miller_rabin_tester(number):
+              # Return false if number not prime
+              return False
 
-    # Check if user input is a prime number
-    result = ""
+        # Return true when most likely prime
+          return True
 
-    if miller_rabin_tester_test(number):
-      result = 1
+        # Check if user input is a prime number
+      result = ""
+
+      if miller_rabin_tester_test(number):
+          result = 1
+      else:
+          result = 0
+
+      # Render page with result
+      print(result)
+      return render_template("miller_rabin_test.html", result=result)
+
     else:
-      result = 0
-
-    # Render page with result
-    print(result)
-    return render_template("miller_rabin_test.html", result=result)
-
-  else:
-    # Render page without result
-    return render_template("miller_rabin_test.html")
+        # Render page without result
+        return render_template("miller_rabin_test.html")
 
 # Prime factors
 @app.route("/prime_factors", methods=["GET", "POST"])
