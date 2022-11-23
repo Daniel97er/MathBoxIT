83bf854cab3217c42a391d068b17eeb2c23f27e3
Daniel97er <108468246+Daniel97er@users.noreply.github.com>
2022-11-23 20:58:53 +0000

    update style

--- app.py
+++ app.py
@@ -71,13 +71,13 @@ def miller_rabin_test():
 
             # Return false if number is apparently not prime
             if number < 2 or number % 2 == 0:
-              return False
+                return False
 
           # Run Miller-Rabin-test 15 times to test
             for i in range(15):
-              if not miller_rabin_tester(number):
-                # Return false if number not prime
-                return False
+                if not miller_rabin_tester(number):
+                    # Return false if number not prime
+                    return False
 
           # Return true when most likely prime
             return True
@@ -102,236 +102,240 @@ def miller_rabin_test():
 @app.route("/prime_factors", methods=["GET", "POST"])
 def prime_factors():
 
-  if request.method == "POST":
+    if request.method == "POST":
 
-    entered_number = int(request.form.get("prime_factor"))
-    
-    def prime_factorization(number):
-        # Return three lists with prime factorials, exponents and one index list for jinja 
-  
+        entered_number = int(request.form.get("prime_factor"))
         
-        # Check zero and one by default
-        if number == 0:
-           # factor, exponent and index
-           return [0], [1], [0]
+        def prime_factorization(number):
+            # Return three lists with prime factorials, exponents and one index list for jinja 
+      
+            
+            # Check zero and one by default
+            if number == 0:
+                # factor, exponent and index
+                return [0], [1], [0]
 
-        if number == 1:
-           # factor, exponent and index
-  	       return [1], [1], [0]
+            if number == 1:
+                # factor, exponent and index
+                return [1], [1], [0]
 
-        # Create two lists for prime factors and exponents
-        factor_list = []
-        exponent_list = []
+            # Create two lists for prime factors and exponents
+            factor_list = []
+            exponent_list = []
 
-        # Get prime numbers up to number
-        prime_list = prime_test(number)
-  
-        # Go through primes and check if it is a prime factor and possibly add to prime factors list
-        for i in prime_list:
-          if number % i == 0:
-            factor_list.append(i)
+            # Get prime numbers up to number
+            prime_list = prime_test(number)
+      
+            # Go through primes and check if it is a prime factor and possibly add to prime factors list
+            for i in prime_list:
+                if number % i == 0:
+                    factor_list.append(i)
 
-        # Calculate the number of exponent for the respective prime factor
-        for i in factor_list:
-          counter = 0
-          temp_number = number
-          # Keep dividing the number until it no longer fits in
-          while temp_number % i == 0:
-            temp_number //= i
-            counter += 1
-          exponent_list.append(counter)
+            # Calculate the number of exponent for the respective prime factor
+            for i in factor_list:
+                counter = 0
+                temp_number = number
+                # Keep dividing the number until it no longer fits in
+                while temp_number % i == 0:
+                    temp_number //= i
+                    counter += 1
+                    exponent_list.append(counter)
 
-        # Create index list for jinja template for loop
-        index_list = [i for i in range(len(factor_list))]
-  
-        return factor_list,exponent_list, index_list
+            # Create index list for jinja template for loop
+            index_list = [i for i in range(len(factor_list))]
+      
+            return factor_list,exponent_list, index_list
 
 
-    def prime_test(number):
-        # Return list of primes up to number (Sieve of Eratosthenes)
+        def prime_test(number):
+            # Return list of primes up to number (Sieve of Eratosthenes)
 
-        prime_test_list = []
+            prime_test_list = []
 
-        # Go through all numbers
-        for i in range(2, number+1):
+            # Go through all numbers
+            for i in range(2, number+1):
 
-         # Set tester to false to check if current i is prime
-         tester = False
-    
-         # Go up to square root because of efficiency
-         for j in range(2,int(math.sqrt(i))+1):
-             # If there is a dividend so i is not prime
-             if i % j == 0:
-                 tester = True
-                 # Stop loop if i is not prime
-                 break
-    
-         # If tester in the end of the loop is false so i is a prime
-         if tester == False:
-             prime_test_list.append(i)
+                # Set tester to false to check if current i is prime
+                tester = False
+        
+                # Go up to square root because of efficiency
+                for j in range(2,int(math.sqrt(i))+1):
+                    # If there is a dividend so i is not prime
+                    if i % j == 0:
+                        tester = True
+                        # Stop loop if i is not prime
+                        break
+            
+                # If tester in the end of the loop is false so i is a prime
+                if tester == False:
+                    prime_test_list.append(i)
 
-  
-        return prime_test_list
-    
-    # Prime factor, prime exponent and index list
-    p_factors, p_exponents, index_list = prime_factorization(entered_number)
+      
+            return prime_test_list
+        
+        # Prime factor, prime exponent and index list
+        p_factors, p_exponents, index_list = prime_factorization(entered_number)
 
-    return render_template("prime_factors.html", p_factors=p_factors, p_exponents=p_exponents, index_list=index_list)
+        return render_template("prime_factors.html", p_factors=p_factors, p_exponents=p_exponents, index_list=index_list)
 
-  else:
-    return render_template("prime_factors.html")
+    else:
+        return render_template("prime_factors.html")
 
 # ISBN-10 TEST
 @app.route("/isbn_test", methods=["GET", "POST"])
 def isbn_test():
   
-  if request.method == "POST":
-    # Get user input
-    number = request.form.get("isbn")
+    if request.method == "POST":
+        # Get user input
+        number = request.form.get("isbn")
 
-    # Check if it is a ten-digit number
-    if len(number) != 10:
+        # Check if it is a ten-digit number
+        if len(number) != 10:
 
-      # Flash message to help the user
-      flash("Please enter a ten-digit ISBN-number without space or other signs")
+            # Flash message to help the user
+            flash("Please enter a ten-digit ISBN-number without space or other signs")
 
-      return render_template("isbn_test.html")
+            return render_template("isbn_test.html")
 
-    for i in number:
-      if not ((ord(i) >= 48 and ord(i) <= 57) or (ord(i) == 88) or (ord(i) == 120)):
-        flash("Not valid sign was entered, please try again")
+        for i in number:
+            if not ((ord(i) >= 48 and ord(i) <= 57) or (ord(i) == 88) or (ord(i) == 120)):
+                flash("Not valid sign was entered, please try again")
 
+                return render_template("isbn_test.html")
+
+        def isbn_test(isbn):
+            # Check if there is a valid ISBN-10 number
+
+            sum = 0
+            counter = 1
+            # Get isbn numbers without check digit
+            isbn_list = isbn[:9]
+
+            # Go through numbers and calculate the value
+            for i in isbn_list:
+                sum += counter*int(i)
+                counter += 1
+
+            # If sum modulo 11 is not equal to check digit it is a not correct isbn
+            result = sum % 11
+
+            temp = -1
+            
+            # Check if check-digit is not int
+            if isbn[9] == "x" or isbn[9] == "X":
+                temp = 10
+            else:
+                temp = int(isbn[9])
+
+            # Return result
+            if result == temp:
+                return 1
+            else:
+                return 0
+
+        result = isbn_test(number)
+
+        return render_template("isbn_test.html", result=result)
+      
+    else:
         return render_template("isbn_test.html")
-
-    def isbn_test(isbn):
-        # Check if there is a valid ISBN-10 number
-
-        sum = 0
-        counter = 1
-        # Get isbn numbers without check digit
-        isbn_list = isbn[:9]
-
-        # Go through numbers and calculate the value
-        for i in isbn_list:
-            sum += counter*int(i)
-            counter += 1
-
-        # If sum modulo 11 is not equal to check digit it is a not correct isbn
-        result = sum % 11
-
-        temp = -1
-        
-        # Check if check-digit is not int
-        if isbn[9] == "x" or isbn[9] == "X":
-          temp = 10
-        else:
-          temp = int(isbn[9])
-
-        # Return result
-        if result == temp:
-            return 1
-        else:
-            return 0
-
-    result = isbn_test(number)
-    return render_template("isbn_test.html", result=result)
-  
-  else:
-    return render_template("isbn_test.html")
 
 
 # Euclidean algorithm
 @app.route("/euclidean_algorithm", methods=["GET", "POST"])
 def euclidean_algorithm():
 
-  if request.method == "POST":
+    if request.method == "POST":
 
-    # Get user data from euclidean_algorithm page
-    number1 = int(request.form.get("euclidean_number1"))
-    number2 = int(request.form.get("euclidean_number2"))
+        # Get user data from euclidean_algorithm page
+        number1 = int(request.form.get("euclidean_number1"))
+        number2 = int(request.form.get("euclidean_number2"))
 
-    def euclidean_algorithm_func(number1, number2):
-        # Return GCD from number1 and number2
+        def euclidean_algorithm_func(number1, number2):
+            # Return GCD from number1 and number2
 
-        # Calculate with modulo while is possible
-        while number2 != 0:
-            temp = number1%number2
-            # swap numbers 
-            number1 = number2
-            number2 = temp
+            # Calculate with modulo while is possible
+            while number2 != 0:
+                temp = number1%number2
+                # swap numbers 
+                number1 = number2
+                number2 = temp
 
-        # Return GCD
-        return number1
+            # Return GCD
+            return number1
 
-    result = euclidean_algorithm_func(number1, number2)
-    
-    return render_template("euclidean_algorithm.html", result=result)
+        result = euclidean_algorithm_func(number1, number2)
+          
+        return render_template("euclidean_algorithm.html", result=result)
 
-  else:
-    return render_template("euclidean_algorithm.html")
+    else:
+        return render_template("euclidean_algorithm.html")
 
 
 # Extended Euclidean algorithm
 @app.route("/extended_euclidean_algorithm", methods=["GET", "POST"])
 def extended_euclidean_algorithm():
 
-  if request.method == "POST":
+    if request.method == "POST":
 
-    # Because of user data
-    try:
-        # Get user data from extended_euclidean_algorithm page
-        number1 = int(request.form.get("extended_euclidean_number1"))
-        number2 = int(request.form.get("extended_euclidean_number2"))
+      # Because of user data
+        try:
+            # Get user data from extended_euclidean_algorithm page
+            number1 = int(request.form.get("extended_euclidean_number1"))
+            number2 = int(request.form.get("extended_euclidean_number2"))
 
-        def extended_euclidean_algorithm(number1, number2):
-            # Extended euclidean algorithm return in this function GCD and linear combination 
+            def extended_euclidean_algorithm(number1, number2):
+                # Extended euclidean algorithm return in this function GCD and linear combination 
 
-            # Avoid error
-            if number2 == 0:
-                return(number1, 1, 0)
+                # Avoid error
+                if number2 == 0:
+                    return(number1, 1, 0)
 
-            # Quotient list 
-            q_list = []
-            # Counter for calculation
-            counter1 = 0
+                # Quotient list 
+                q_list = []
+                # Counter for calculation
+                counter1 = 0
 
-            # GCD calculation and documentation
-            while number2 != 0:
-                q_list.append(number1 // number2)
-                number3 = number2
-                number2 = number1 % number2
-                number1 = number3
-                counter1 += 1
+                # GCD calculation and documentation
+                while number2 != 0:
+                    q_list.append(number1 // number2)
+                    number3 = number2
+                    number2 = number1 % number2
+                    number1 = number3
+                    counter1 += 1
 
-            # Variable for the backward algorithm
-            x = 0
-            y = 1
-            # List of y
-            y_list_temp = [1]
-            # counter2 to get the right index from list
-            counter2 = 0
+                # Variable for the backward algorithm
+                x = 0
+                y = 1
+                # List of y
+                y_list_temp = [1]
+                # counter2 to get the right index from list
+                counter2 = 0
 
-            # Go backward through list and calculate the linear combinations
-            while counter1 > 1:
-                # Calculate y and add in the list then calculate x
-                y = x - q_list[counter1 - 2] * y_list_temp[counter2]
-                y_list_temp.append(y)
-                x = y_list_temp[counter2]
-                # Update counters
-                counter2 += 1
-                counter1 -= 1
-    
+                # Go backward through list and calculate the linear combinations
+                while counter1 > 1:
+                    # Calculate y and add in the list then calculate x
+                    y = x - q_list[counter1 - 2] * y_list_temp[counter2]
+                    y_list_temp.append(y)
+                    x = y_list_temp[counter2]
+                    # Update counters
+                    counter2 += 1
+                    counter1 -= 1
+        
 
-            return(number1, x, y)
+                return(number1, x, y)
 
-        result, result_number1, result_number2 = extended_euclidean_algorithm(number1, number2)
+            result, result_number1, result_number2 = extended_euclidean_algorithm(number1, number2)
 
-        return render_template("extended_euclidean_algorithm.html", number1=number1, number2=number2, result=result, result_number1=result_number1, result_number2=result_number2)
-    except:
-      flash("Please enter number in the number fields")
-      return render_template("extended_euclidean_algorithm.html")
-  else:
-    return render_template("extended_euclidean_algorithm.html")
+            return render_template("extended_euclidean_algorithm.html", number1=number1, number2=number2, result=result, result_number1=result_number1, result_number2=result_number2)
+        
+        except:
+
+            flash("Please enter number in the number fields")
+          
+            return render_template("extended_euclidean_algorithm.html")
+    else:
+        return render_template("extended_euclidean_algorithm.html")
 
 # Decimal to numerial system
 @app.route("/decimal_to_numeral_system", methods=["GET", "POST"])
